cc676b8a879d48c0d0f3d62325c34ceac6968a4d
Simon Sadedin <ssadedin@gmail.com>
2015-06-11 20:54:40 +1000

    support for labeling y axis support for plots that are segmented into disjointed regions support for drawing smoothed curves via loess smoothing support for computing color of lines on the fly via closure callback support for plot margins support for drawing borders on plots support for controlling the size of ticks on bars support for drawing dashed lines support for switching to direct pixel coordinates instead of logical plot coordinates (useful for labelling axes, etc)

--- src/main/groovy/graxxia/Drawing.groovy
+++ src/main/groovy/graxxia/Drawing.groovy
@@ -12,12 +12,17 @@ package graxxia
 
 import groovy.transform.CompileStatic;
 
+import java.awt.BasicStroke
 import java.awt.Font
 import java.awt.FontMetrics
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Color;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+
+import org.apache.commons.math3.analysis.interpolation.*
+import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
 
 import javax.imageio.*
 
@@ -30,19 +35,27 @@ import javax.imageio.*
  */
 class Drawing {
     
+    static enum Coord {
+        PIXELS,
+        PLOT
+    }
+    
     BufferedImage  image = null;
     Graphics2D g = null
     String fileName = null
     
     List<String> operations = []
     
+    boolean autoSave = true
+    
     int xOffset = 0
     int yOffset = 0
     
-    double maxX = 0
-    double maxY = 0
-    double minX = 0
     double minY = 0
+    double maxY = 0
+    
+    List minX = []
+    List maxX = []
     
     double xScale = 1.0d
     double yScale = 1.0d
@@ -55,6 +68,28 @@ class Drawing {
     Font font = null
     
     FontMetrics fontMetrics = null
+    
+    /**
+     * The default coordinate mode is to specify points in plot coordinates,
+     * as opposed to pixels.
+     */
+    Coord coordType = Coord.PLOT
+    
+    static float [] dash1 = [5.0f] as float[];
+    
+    /**
+     * The default line style used for dashed lines
+     */
+    BasicStroke DASHED =
+        new BasicStroke(1.0f,
+                        BasicStroke.CAP_BUTT,
+                        BasicStroke.JOIN_MITER,
+                        10.0f, dash1, 0.0f);
+    
+    /**
+     * The default line style used for solid lines                    
+     */
+    BasicStroke SOLID = new BasicStroke(1.0f)                   
     
     /**
      * The default y position at which the next line of text will be drawn
@@ -71,16 +106,39 @@ class Drawing {
      */
     double textHeight = 0
     
+    int xMargin = 0
+    
+    int yMargin = 0
+    
     public Drawing(String fileName, int width, int height, double minX, double minY, double maxX, double maxY) {
-        this.image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        this.g = image.createGraphics()
+        this(fileName, width, height, [minX], minY, [maxX], maxY)
+    }
+    
+    public Drawing(String fileName, int width, int height, List minX, double minY, List maxX, double maxY) {
         this.fileName = fileName
-        this.xScale = width / (maxX - minX)
-        this.yScale = height / (maxY - minY) // invert Y so it is like math plotting coordinates
-        this.xOffset = -minX 
-        this.yOffset = -minY 
+        this.minX = minX
+        this.maxX = maxX
+        this.minY = minY
+        this.maxY = maxY
         this.height = height
         this.width = width
+        this.init()
+    }
+    
+    void setMargin(int x, int y) {
+        this.xMargin = x
+        this.yMargin = y
+        this.init()
+    }
+    
+    void init() {
+        this.image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        this.g = image.createGraphics()
+        this.xScale = (width-2*xMargin) / ([maxX,minX].transpose().collect { it[0] - it[1] }.sum() )
+        this.yScale = (height-2*yMargin) / (maxY - minY) // invert Y so it is like math plotting coordinates
+        this.xOffset = -(minX.min()) 
+        this.yOffset = -minY 
+
         g.setBackground(Color.WHITE)
         g.clearRect(0,0,width,height)
         color(0,0,0)
@@ -90,7 +148,7 @@ class Drawing {
             RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         
         setFont("SansSerif", Font.PLAIN, 12);
-        textY = maxY
+        textY = maxY 
     }
     
     public Drawing(String fileName, int width, int height) {
@@ -137,6 +195,43 @@ class Drawing {
     Drawing bar(IntRange range, def y, String overText=null, String underText=null) {
         bar(range.from, range.to, y, overText, underText)
     }
+    
+    
+    PolynomialSplineFunction loess(Map options=[:], Iterable<Double> x, Iterable<Double> y) {
+        LoessInterpolator interp = new LoessInterpolator()
+        PolynomialSplineFunction fn = interp.interpolate(x as double[], y as double[])
+        
+        double minXValue = x.min()
+        double maxXValue = x.max()
+        
+        // Interpolate to factor of 10
+        double width = maxXValue - minXValue
+        
+        // A point every 5 pixels
+        double xInterval = 5 / xScale
+        double xInterp = minXValue
+        List lineYValues = []
+        List lineXValues = []
+        List colors = []
+        while(xInterp < maxXValue) {
+            lineXValues << xInterp
+            double yValue = fn.value(xInterp)
+            lineYValues << yValue
+            
+            if(options.color != null)
+                colors << options.color(xInterp, yValue)
+            
+            xInterp += xInterval
+        }
+        
+        if(options.color != null) {
+            options.color = colors;
+        }
+        
+        lines(options, lineXValues[0..-2], lineYValues[0..-2], lineXValues[1..-1], lineYValues[1..-1])
+        
+        return fn
+    }
       
     Drawing lines(Map options=[:], def x1, def y1, def x2, def y2) {
 		if(x1 instanceof List ||x1 instanceof double[]) {
@@ -148,14 +243,21 @@ class Drawing {
 		}
                 
         x1.eachWithIndex { x, index -> 
-            if(options.color)
-                color(options.color)
+            if(options.color) {
+                if(options.color instanceof Closure)
+                    color(options.color(x, y1[index], x2[index], y2[index]))
+                else
+                if(options.color instanceof List)
+                    color(options.color[index])
+                else
+                    color(options.color)
+            }
                 
             def txed = txLine(x, y1[index], x2[index], y2[index]) 
             if(options.bars) {
                 drawBar(txed, options.overText?options.overText[index]:null, options.underText?options.underText[index]:null)
                 
-                if(options.overText) {
+                if(options.overText && options.overText[index]) {
                     // Draw text centered over bar with baseline 1 pixel above bar
                     int textWidth = fontMetrics.stringWidth(options.overText[index])
                     int middleX = txed[0] + (txed[2]-txed[0])/2
@@ -163,7 +265,7 @@ class Drawing {
                     g.drawString(options.overText[index],(float)textXPos, (float)(txed[1] - 4))
                 }
                 
-                if(options.underText) {
+                if(options.underText && options.underText[index]) {
                     // Draw text centered over bar with baseline 1 pixel above bar
                     int textWidth = fontMetrics.stringWidth(options.underText[index])
                     int middleX = txed[0] + (txed[2]-txed[0])/2
@@ -172,19 +274,30 @@ class Drawing {
                 } 
             }
         }
-        save()
+        if(autoSave)
+            save()
         return this
     }
     
+    int barHeightUp = 5
+    
+    int barHeightDown = 5
+    
     private void drawBar(lineCoords,String overText=null, String underText=null) {
-        g.drawLine((int)lineCoords[0],(int)lineCoords[1]-5,(int)lineCoords[0],(int)lineCoords[1]+5)
-        g.drawLine((int)lineCoords[2],(int)lineCoords[3]-5,(int)lineCoords[2],(int)lineCoords[3]+5)
+        g.drawLine((int)lineCoords[0],(int)lineCoords[1]-barHeightUp,(int)lineCoords[0],(int)lineCoords[1]+barHeightDown)
+        g.drawLine((int)lineCoords[2],(int)lineCoords[3]-barHeightUp,(int)lineCoords[2],(int)lineCoords[3]+barHeightDown)
     }
     
     Drawing line(def x1, def y1, def x2, def y2) {
+//        println "line ($x1,$y1) - ($x2,$y2)"
         txLine(x1,y1,x2,y2)
-        save()      
+        if(autoSave)
+            save()      
         return this
+    }
+    
+    Drawing color(List rgb) {
+        color(rgb[0],rgb[1], rgb[2])
     }
     
     Drawing color(String value) {
@@ -199,15 +312,58 @@ class Drawing {
     }
     
     List<Double> txLine(double x1, double y1, double x2, double y2) {
-        x1 = xScale * (xOffset + x1)
-        x2 = xScale * (xOffset + x2)
-        y1 = this.height - yScale * (yOffset + y1)
-        y2 = this.height - yScale * (yOffset + y2)
+        
+        double x1Tx = txX(x1)
+        double x2Tx = txX(x2)
+        
+        y1 = txY(y1)
+        y2 = txY(y2)
         
         if(log)
-            println "Drawing line ($x1,$y1) - ($x2,$y2)"
-        g.drawLine((int)x1, (int)y1, (int)x2, (int)y2)
-        [x1,y1,x2,y2]
+            println "Drawing line ($x1-$x2) ($x1Tx,$y1) - ($x2Tx,$y2)"
+        g.drawLine((int)x1Tx, (int)y1, (int)x2Tx, (int)y2)
+        [x1Tx,y1,x2Tx,y2]
+    }
+    
+    void drawRegions() {
+        [minX,maxX].transpose().each { region ->
+            line(region[0],minY,region[0],maxY)
+            line(region[1],minY,region[1],maxY)
+        }
+    }
+    
+    void drawBorder() {
+        g.drawRect(xMargin-1, yMargin-1, width-2*xMargin+2, height-2*yMargin+2)
+    }
+    
+    Double txX(double x) {
+        
+        if(coordType == Coord.PIXELS)
+            return x
+        
+        double startOffset = xMargin;
+        int xInterval = 0
+        while(x > maxX[xInterval]) {
+            startOffset += (xScale * (maxX[xInterval] - minX[xInterval]))
+            
+            ++xInterval    
+            
+            if(xInterval >= maxX.size() -1)
+                break
+        }
+        if(xInterval >= minX.size())
+            xInterval = minX.size()-1
+            
+        Double result = startOffset + xScale *(Math.max(x - minX[xInterval],0))
+        
+//        println "$x => $result"
+        return result
+    }
+    
+    Double txY(double y) {
+        if(coordType == Coord.PIXELS)
+            return y
+        (this.height - yMargin) - yScale * (yOffset + y) 
     }
     
     @CompileStatic
@@ -224,6 +380,14 @@ class Drawing {
         ImageIO.write(image, "png", new File(fileName));
     }
     
+    void save(Closure c) {
+        boolean oldAutoSave = this.autoSave
+        this.autoSave = false
+        c()
+        save()
+        this.autoSave = oldAutoSave
+    }
+    
     /**
      * Draws text at the given x,y position in graph coordinates (not pixels)
      * <p>
@@ -237,12 +401,13 @@ class Drawing {
         textY = y + this.textHeight
         textX = x
         value = String.valueOf(value)
-        x = xScale * (xOffset + x)
-        y = this.height - yScale * (yOffset + y)
+        x = txX(x)
+        y = txY(y)
         if(log)
             println "Drawing $value at ($x,$y)"
         g.drawString(value, (float)x, (float)y)
-        save()
+        if(autoSave)
+            save()
     }
     
     /**
@@ -258,6 +423,25 @@ class Drawing {
         double x = textX.toDouble()
         double y = textY.toDouble()
         text(x,y,value)
+    }
+    
+    Drawing drawYAxis(List yAxis, boolean grid=false) {
+                   
+        def yTickPoints = yAxis.collect { txY(it) }
+            
+        save {
+            g.setStroke(DASHED)
+            lines([minX[0]] * (yAxis.size()-2), yAxis[1..-2], [maxX[-1]]*(yAxis.size()-2), yAxis[1..-2])
+            g.setStroke(SOLID)
+                
+            color("black")
+            coordType = Drawing.Coord.PIXELS
+            [yAxis, yTickPoints].transpose().each {
+                text(xMargin-40, it[1], it[0])
+            }
+        }
+        coordType = Drawing.Coord.PLOT
+        return this
     }
     
     static void main(String [] args) {
