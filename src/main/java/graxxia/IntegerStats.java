cb2285eb0ff18dcf89072732d948d11e66bee4a8
Simon Sadedin <ssadedin@gmail.com>
2018-05-01 10:23:10 +1000

    add license header

--- src/main/java/graxxia/IntegerStats.java
+++ src/main/java/graxxia/IntegerStats.java
@@ -1,3 +1,13 @@
+/*
+ *  Graxxia - Groovy Maths Utilities
+ *
+ *  Copyright (C) 2014 Simon Sadedin, ssadedin<at>gmail.com and contributors.
+ *
+ *  This file is licensed under the Apache Software License Version 2.0.
+ *  For the avoidance of doubt, it may be alternatively licensed under GPLv2.0
+ *  and GPLv3.0. Please see LICENSE.txt in your distribution directory for
+ *  further details.
+ */
 package graxxia;
 
 import java.io.BufferedReader;
