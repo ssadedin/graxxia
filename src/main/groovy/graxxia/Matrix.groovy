971dba8f18eeb011aa392cfe89234a0c85989947
Simon Sadedin <simon.sadedin@mcri.edu.au>
2025-12-11 10:42:15 +1100

    fix index OOB exception when initializing directly from array

--- src/main/groovy/graxxia/Matrix.groovy
+++ src/main/groovy/graxxia/Matrix.groovy
@@ -456,7 +456,7 @@ class Matrix extends Expando implements Iterable, Serializable {
         int i=0
         for(int r=0; r<rows; ++r) {
             for(int c=0; c<columns;++c) {
-                matrix.dataRef[r][c] = matrixData[++i]
+                matrix.dataRef[r][c] = matrixData[i++]
             }
         }
         
